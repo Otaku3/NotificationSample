74a5a5a353e058c11832cc8f8e7a958e9eac1225
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2018-10-19 13:43:00 +0900

    Initial Commit

---
+++ NotificationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationSample
+//
+//  Created by 大林拓実 on 2018/10/19.
+//  Copyright © 2018年 TakumiObayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
